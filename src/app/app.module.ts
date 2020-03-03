c61fc9539d5c411fb53620d23d6dbb8657b20c14
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-03-03 11:54:43 +0200

    New proj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
