967f5fdfceb1002ba83591fab3a1054ff6268cac
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-03-10 10:51:26 +0200

    Созданный проект

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../shared/user.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ar-sign-in',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  isLoginError: boolean = false;
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  OnSubmit(userName, password) {
+    this.userService.userAuthentication(userName, password)
+      .subscribe((data: any) => {
+        console.log(data);
+        localStorage.setItem('userToken', data.access_token);
+        this.router.navigate(['/home']);
+      }, (err: HttpErrorResponse) => {
+        console.log(err);
+        this.isLoginError = true;
+        });
   }
 
 }
